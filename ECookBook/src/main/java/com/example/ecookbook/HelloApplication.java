55d54e4ceee274fcee811f39c7dcab2d0e04a5e2
Nil Özdemir <120586927+nil00007@users.noreply.github.com>
2024-05-01 15:49:49 +0300

    Update HelloApplication.java

--- ECookBook/src/main/java/com/example/ecookbook/HelloApplication.java
+++ ECookBook/src/main/java/com/example/ecookbook/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1315, 890);
-        stage.setTitle("Cook Book");
+        stage.setTitle("E-CookBook");
         stage.setScene(scene);
         stage.show();
     }
